74722d40686623364ba5dc32f3fc4e21733dd45c
Peter Abahim <lilpero03@gmail.com>
2025-08-02 02:08:45 +0100

    feature i justmade the mobile menu funtional

--- app/HeroSection/heroPage.tsx
+++ app/HeroSection/heroPage.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HeroPage = () => {
   return (
-    <div className="lg:py-20 px-5 lg:px-60">
+    <div className="lg:py-20 pt-14 px-5 lg:px-60">
     <div className="overflow-hidden rounded-xl  lg:rounded-3xl">
       <video
         autoPlay
@@ -16,7 +16,7 @@ const HeroPage = () => {
 
     <div className='relative bottom-44 lg:bottom-[800px] lg:px-14 px-4'>
       <h1 className='lg:text-[130px] text-2xl font-medium w-[1px] text-white lg:w-4 lg:leading-38 lg:pt-4'>Dream Invest Live</h1>
-      <button className='bg-[#e6ff55] text-green-700 py- px-2 lg:p-7 lg:px-12 rounded-xl lg:text-4xl lg:mt-20 mt-5'> Start investing</button>
+      <button className='bg-[#e6ff55] text-green-700 py-1 px-2 lg:p-7 lg:px-12 rounded-xl lg:text-4xl lg:mt-20 mt-5'> Start investing</button>
     </div>
   </div>
   
--- app/Navbar/navPage.tsx
+++ app/Navbar/navPage.tsx
@@ -1,9 +1,16 @@
+'use client'
+import { useState } from 'react'
 import React from 'react'
 import Image from 'next/image'
 import img from '@/public/logo.png'
 import { IoMenu } from "react-icons/io5";
+import { MdClose } from "react-icons/md";
 
 const navPage = () => {
+    const[isClick, setIsClick] = useState(false);
+    const handleClick = () => {
+        setIsClick(!isClick);
+    }
   return (
     <div>
         <nav className='flex justify-between items-center p-5 lg:px-60 bg-white '>
@@ -24,8 +31,20 @@ const navPage = () => {
                 <button className='bg-green-500 hidden hover:scale-100 text-white font-semibold rounded-md  py-2 lg:flex px-3'>Get Started</button>
             </div>
 
-            <IoMenu className='text-2xl lg:hidden'/>
+            < span onClick={handleClick} className='text-2xl lg:hidden'>{isClick ? < MdClose/> : <IoMenu/>}</span>
         </nav>
+
+        <div>
+                <ul className={isClick ? ' lg:hidden pt-5 flex flex-col gap-7 text-center text-gray-500 font-medium' : "hidden"}>
+                    <li className='cursor-pointer hover:scale-100'>Products</li>
+                    <li className='cursor-pointer hover:scale-100'>Learn</li>
+                    <li className='cursor-pointer hover:scale-100'>Community</li>
+                    <li className='cursor-pointer hover:scale-100'>FAQ</li>
+                    <div>
+                    <button className='bg-green-500 lg:hidden hover:scale-100 text-white text-sm font-semibold rounded-md  py-2 text-center px-28 mt-5'>Get Started</button>
+                    </div>
+                </ul>
+            </div>
     </div>
   )
 }
